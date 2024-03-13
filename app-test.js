f91d572d10670fe7d2b9d9b58bcba0e41698c55d
gmatta7 <111488165+gmatta7@users.noreply.github.com>
2024-03-13 14:51:43 +1100

    removed the error in the test cases

--- app-test.js
+++ app-test.js
@@ -21,8 +21,8 @@ describe('Planets API Suite', () => {
               .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.have.property('id').eql(1);
-                    // res.body.should.have.property('name').eql('Mercury');
-                    res.body.should.have.property('name').eql('Mercury-ERROR');
+                    res.body.should.have.property('name').eql('Mercury');
+                    // res.body.should.have.property('name').eql('Mercury-ERROR');
                 done();
               });
         });
